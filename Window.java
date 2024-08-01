b2eef9d7f746a19a7710568ba544295d4102cd98
Murat65536 <bmuratb502@gmail.com>
2024-08-01 15:24:14 -0400

    Change typing.

--- Window.java
+++ Window.java
@@ -19,24 +19,24 @@ import javax.swing.JPanel;
 import javax.swing.Timer;
 
 public class Window extends JPanel implements ActionListener, KeyListener {
-  private static final int WIDTH = 960;
-  private static final int HEIGHT = 640;
+  private static final short WIDTH = 960;
+  private static final short HEIGHT = 640;
   private final BufferedImage bufferedImage;
   private final JLabel jLabel = new JLabel();
   private final Timer timer = new Timer(10, this);
-  private int playerX = 150;
-  private int playerY = 400;
-  private int mapX = 8;
-  private int mapY = 8;
-  private int mapSize = 64;
+  private short playerX = 150;
+  private short playerY = 400;
+  private byte mapX = 8;
+  private byte mapY = 8;
+  private short mapSize = 64;
   private Scanner textureColorFile;
-  private int[] textureColors = new int[27648];
+  private short[] textureColors = new short[27648];
   private Scanner skyColorFile;
-  private int[] skyColors = new int[28800];
+  private short[] skyColors = new short[28800];
   private ArrayList<Integer> keys = new ArrayList<Integer>();
   private ArrayList<ArrayList<Integer>> sprites = new ArrayList<ArrayList<Integer>>();
 
-  private int[][] wallMap = {
+  private byte[][] wallMap = {
     {1, 1, 1, 1, 1, 1, 2, 1},
     {1, 0, 1, 0, 0, 0, 2, 1},
     {1, 0, 1, 0, 0, 0, 4, 1},
@@ -47,7 +47,7 @@ public class Window extends JPanel implements ActionListener, KeyListener {
     {1, 1, 1, 1, 1, 1, 1, 1}
   };
 
-  private int[][] floorMap = {
+  private byte[][] floorMap = {
     {0, 0, 0, 0, 0, 0, 0, 0},
     {0, 0, 0, 0, 1, 1, 0, 0},
     {0, 0, 0, 0, 2, 0, 0, 0},
@@ -58,7 +58,7 @@ public class Window extends JPanel implements ActionListener, KeyListener {
     {0, 0, 0, 0, 0, 0, 0, 0}
   };
 
-  private int[][] ceilingMap = {
+  private byte[][] ceilingMap = {
     {0, 0, 0, 0, 0, 0, 0, 0},
     {0, 0, 0, 0, 0, 0, 0, 0},
     {0, 0, 0, 0, 0, 0, 0, 0},
@@ -72,11 +72,7 @@ public class Window extends JPanel implements ActionListener, KeyListener {
   private double playerDeltaX = Math.cos(Math.toRadians(playerAngle));
   private double playerDeltaY = -Math.sin(Math.toRadians(playerAngle));
 
-  private long lastTime = System.nanoTime();
-  private int fps;
-  private int frames;
-
-  private int[] depth = new int[120];
+  private short[] depth = new short[120];
 
   public Window() {
       super(true);
@@ -86,58 +82,51 @@ public class Window extends JPanel implements ActionListener, KeyListener {
       jLabel.setIcon(new ImageIcon(bufferedImage));
       this.add(jLabel);
       appendTextures();
-      sprites.add(new ArrayList<Integer>(Arrays.asList(1, 1, 0, (int)(1.5 * 64), 5 * 64, 20)));
+      sprites.add(new ArrayList<Integer>(Arrays.asList(1, 1, 0, 2 * 64, 5 * 64, 20)));
       timer.start();
-  }
-
-  @Override
-  public void actionPerformed(ActionEvent e) {
-    movePlayer();
-    clear();
-    drawSky();
-    drawScene();
-    drawSprites();
-    jLabel.repaint();
-    frames++;
-    if (System.nanoTime() > lastTime + 1000000000) {
-      lastTime = System.nanoTime();
-      fps = frames;
-      frames = 0;
-      System.out.println(fps);
-    }
-  }
-
-  private void appendTextures() {
-    int line;
-    try {
-      textureColorFile = new Scanner(new File("textures.txt"));
-    }
-    catch (FileNotFoundException error) {
-      error.printStackTrace();
-    }
-    line = 0;
-    while (textureColorFile.hasNextInt()) {
-      textureColors[line++] = textureColorFile.nextInt();
     }
     
+    @Override
+    public void actionPerformed(ActionEvent e) {
+      Graphics2D graphics = bufferedImage.createGraphics();
+      movePlayer();
+      clear(graphics);
+      drawSky(graphics);
+      drawScene(graphics);
+      // drawSprites(graphics);
+      jLabel.repaint();
+      graphics.dispose();
+    }
+
+  private void appendTextures() {
+    short line = 0;
     try {
-      skyColorFile = new Scanner(new File("sky.txt"));
+      textureColorFile = new Scanner(new File("textures.txt"));
+      while (textureColorFile.hasNextInt()) {
+        textureColors[line++] = textureColorFile.nextShort();
+      }
     }
     catch (FileNotFoundException error) {
       error.printStackTrace();
     }
+    
     line = 0;
-    while (skyColorFile.hasNextInt()) {
-      skyColors[line++] = skyColorFile.nextInt();
+
+    try {
+      skyColorFile = new Scanner(new File("sky.txt"));
+      while (skyColorFile.hasNextInt()) {
+        skyColors[line++] = skyColorFile.nextShort();
+      }
+      skyColorFile.close();
     }
-    skyColorFile.close();
+    catch (FileNotFoundException error) {
+      error.printStackTrace();
+    }
   }
   
-  private void clear() {
-    Graphics2D g = bufferedImage.createGraphics();
-    g.setColor(new Color(50, 50, 50));
-    g.fillRect(0, 0, WIDTH, HEIGHT);
-    g.dispose();
+  private void clear(Graphics2D graphics) {
+    graphics.setColor(new Color(50, 50, 50));
+    graphics.fillRect(0, 0, WIDTH, HEIGHT);
   }
 
   private double fixAngle(double angle) {
@@ -164,8 +153,8 @@ public class Window extends JPanel implements ActionListener, KeyListener {
       playerDeltaY = -Math.sin(Math.toRadians(playerAngle));
     }
 
-    int xOffset = 0;
-    int yOffset = 0;
+    byte xOffset = 0;
+    byte yOffset = 0;
     if (playerDeltaX < 0) {
       xOffset = -20;
     }
@@ -213,12 +202,11 @@ public class Window extends JPanel implements ActionListener, KeyListener {
   }
   public void keyTyped(KeyEvent event) {}
 
-  private void drawScene() {
-    Graphics2D g = bufferedImage.createGraphics();
-    int mapArrayX;
-    int mapArrayY;
-    int mapPosition;
-    int depthOfField;
+  private void drawScene(Graphics2D graphics) {
+    short mapArrayX;
+    short mapArrayY;
+    short mapPosition;
+    byte depthOfField;
     double rayX = 0;
     double rayY = 0;
     double rayAngle = fixAngle(playerAngle + 30);
@@ -226,20 +214,20 @@ public class Window extends JPanel implements ActionListener, KeyListener {
     double yOffset = 0;
     double verticalX = 0;
     double verticalY = 0;
-    for (byte rays = 0; rays < 120; rays++) {
-      int verticalMapTexture = 0;
-      int horizontalMapTexture = 0;
+    for (short rays = 0; rays < 120; rays++) {
+      short verticalMapTexture = 0;
+      short horizontalMapTexture = 0;
       depthOfField = 0;
       double verticalDistance = 1000000;
       double tan = Math.tan(Math.toRadians(rayAngle));
       if (Math.cos(Math.toRadians(rayAngle)) > 0.001) {
-        rayX = (((int)playerX >> 6) << 6) + 64;
+        rayX = ((playerX >> 6) << 6) + 64;
         rayY = (playerX - rayX) * tan + playerY;
         xOffset = 64;
         yOffset = -xOffset * tan;
       }
       else if (Math.cos(Math.toRadians(rayAngle)) < -0.001) {
-        rayX = (((int)playerX >> 6) << 6) - 0.0001;
+        rayX = ((playerX >> 6) << 6) - 0.0001;
         rayY = (playerX - rayX) * tan + playerY;
         xOffset = -64;
         yOffset = -xOffset * tan;
@@ -250,10 +238,10 @@ public class Window extends JPanel implements ActionListener, KeyListener {
         depthOfField = 8;
       }
       while (depthOfField < 8) {
-        mapArrayX = (int)(rayX) >> 6;
-        mapArrayY = (int)(rayY) >> 6;
+        mapArrayX = (short)((short)(rayX) >> 6);
+        mapArrayY = (short)((short)(rayY) >> 6);
         if (mapArrayY > 0 && mapArrayY < mapY && wallMap[mapArrayY][mapArrayX] > 0) {
-          verticalMapTexture = wallMap[mapArrayY][mapArrayX] - 1;
+          verticalMapTexture = (short)(wallMap[mapArrayY][mapArrayX] - 1);
           depthOfField = 8;
           verticalDistance = Math.cos(Math.toRadians(rayAngle)) * (rayX - playerX) - Math.sin(Math.toRadians(rayAngle)) * (rayY - playerY);
         }
@@ -270,13 +258,13 @@ public class Window extends JPanel implements ActionListener, KeyListener {
       double horizontalDistance = 1000000;
       tan = 1 / tan;
       if (Math.sin(Math.toRadians(rayAngle)) > 0.001) {
-        rayY = (((int)playerY >> 6) << 6) - 0.0001;
+        rayY = ((playerY >> 6) << 6) - 0.0001;
         rayX = (playerY - rayY) * tan + playerX;
         yOffset = -64;
         xOffset = -yOffset * tan;
       }
       else if (Math.sin(Math.toRadians(rayAngle)) < -0.001) {
-        rayY = (((int)playerY >> 6) << 6) + 64;
+        rayY = ((playerY >> 6) << 6) + 64;
         rayX = (playerY - rayY) * tan + playerX;
         yOffset = 64;
         xOffset = -yOffset * tan;
@@ -287,10 +275,10 @@ public class Window extends JPanel implements ActionListener, KeyListener {
         depthOfField = 8;
       }
       while (depthOfField < 8) {
-        mapArrayX = (int)(rayX) >> 6;
-        mapArrayY = (int)(rayY) >> 6;
+        mapArrayX = (short)((short)(rayX) >> 6);
+        mapArrayY = (short)((short)(rayY) >> 6);
         if (mapArrayX > 0 && mapArrayX < mapX && wallMap[mapArrayY][mapArrayX] > 0) {
-          horizontalMapTexture = wallMap[mapArrayY][mapArrayX] - 1;
+          horizontalMapTexture = (short)(wallMap[mapArrayY][mapArrayX] - 1);
           depthOfField = 8;
           horizontalDistance = Math.cos(Math.toRadians(rayAngle)) * (rayX - playerX) - Math.sin(Math.toRadians(rayAngle)) * (rayY - playerY);
         }
@@ -313,21 +301,21 @@ public class Window extends JPanel implements ActionListener, KeyListener {
       
       double cameraAngle = fixAngle(playerAngle - rayAngle);
       horizontalDistance = horizontalDistance * Math.cos(Math.toRadians(cameraAngle));
-      int lineHeight = (int)((mapSize * 640) / (horizontalDistance));
+      short lineHeight = (short)((mapSize * 640) / (horizontalDistance));
       double textureYStep = 32.0 / lineHeight;
       double textureYOffset = 0;
       if (lineHeight > 640) {
         textureYOffset = (lineHeight - 640) / 2;
         lineHeight = 640;
       }
-      int lineOffset = 320 - (lineHeight >> 1);
+      short lineOffset = (short)(320 - (lineHeight >> 1));
 
-      depth[rays] = (int)horizontalDistance;
+      depth[rays] = (short)horizontalDistance;
 
       double textureY = textureYOffset * textureYStep;
       double textureX;
       if (shade == 1) {
-        textureX = (int)(rayX / 2) % 32;
+        textureX = (rayX / 2) % 32;
         if (rayAngle > 180) {
           if (rayAngle > 180) {
             textureX = 31 - textureX;
@@ -335,72 +323,68 @@ public class Window extends JPanel implements ActionListener, KeyListener {
         }
       }
       else {
-        textureX = (int)(rayY / 2) % 32;
+        textureX = (rayY / 2) % 32;
         if (rayAngle > 90 && rayAngle < 270) {
           textureX = 31 - textureX;
         }
       }
-      for (int y = 0; y < lineHeight; y++) {
-        int pixel = ((int)textureY * 32 + (int)textureX) * 3 + (horizontalMapTexture * 32 * 32 * 3);
+      for (short y = 0; y < lineHeight; y++) {
+        short pixel = (short)((short)((short)textureY * 32 + textureX) * 3 + ((short)horizontalMapTexture * 32 * 32 * 3));
         double red = textureColors[pixel] * shade;
         double green = textureColors[pixel + 1] * shade;
         double blue = textureColors[pixel + 2] * shade;
-        g.setColor(new Color((int)red, (int)green, (int)blue));
-        g.fillRect(rays * 8, y + lineOffset, 8, 8);
+        graphics.setColor(new Color((short)red, (short)green, (short)blue));
+        graphics.fillRect(rays * 8, y + lineOffset, 8, 8);
         textureY += textureYStep;
       }
-      for (int y = lineOffset + lineHeight; y < 640; y++) {
+      for (short y = (short)(lineOffset + lineHeight); y < 640; y++) {
         double yDegrees = y - (640 / 2);
         double degrees = Math.toRadians(rayAngle);
         double rayAngleFix = Math.cos(Math.toRadians(fixAngle(playerAngle - rayAngle)));
         textureX = playerX / 2 + Math.cos(degrees) * 158 * 2 * 32 / yDegrees / rayAngleFix;
         textureY = playerY / 2 - Math.sin(degrees) * 158 * 2 * 32 / yDegrees / rayAngleFix;
-        mapPosition = floorMap[(int)(textureY / 32)][(int)(textureX / 32)] * 32 * 32;
-        int pixel = (((int)textureY & 31) * 32 + ((int)textureX & 31)) * 3 + mapPosition * 3;
+        mapPosition = (short)(floorMap[(short)(textureY / 32)][(short)(textureX / 32)] * 32 * 32);
+        short pixel = (short)((((short)textureY & 31) * 32 + ((short)textureX & 31)) * 3 + mapPosition * 3);
         double red = textureColors[pixel] * 0.7;
         double green = textureColors[pixel + 1] * 0.7;
         double blue = textureColors[pixel + 2] * 0.7;
-        g.setColor(new Color((int)red, (int)green, (int)blue));
-        g.fillRect(rays * 8, y, 8, 1);
+        graphics.setColor(new Color((short)red, (short)green, (short)blue));
+        graphics.fillRect(rays * 8, y, 8, 1);
         textureY += textureYStep;
         
-        mapPosition = ceilingMap[(int)(textureY / 32)][(int)(textureX / 32)] * 32 * 32;
+        mapPosition = (short)(ceilingMap[(short)(textureY / 32)][(short)(textureX / 32)] * 32 * 32);
         if (mapPosition > 0) {
-          pixel = (((int)textureY & 31) * 32 + ((int)textureX & 31)) * 3 + mapPosition * 3;
+          pixel = (short)((((short)textureY & 31) * 32 + ((short)textureX & 31)) * 3 + mapPosition * 3);
           red = textureColors[pixel];
           green = textureColors[pixel + 1];
           blue = textureColors[pixel + 2];
-          g.setColor(new Color((int)red, (int)green, (int)blue));
-          g.fillRect(rays * 8, 640 - y, 8, 1);
+          graphics.setColor(new Color((short)red, (short)green, (short)blue));
+          graphics.fillRect(rays * 8, 640 - y, 8, 1);
         }
       }
       rayAngle = fixAngle(rayAngle - 0.5);
     }
-    g.dispose();
   }
 
-  private void drawSky() {
-    Graphics2D g = bufferedImage.createGraphics();
+  private void drawSky(Graphics2D graphics) {
     for (byte y = 0; y < 40; y++) {
       for (byte x = 0; x < 120; x++) {
-        int xOffset = (int)playerAngle * 2 - x;
+        short xOffset = (short)(playerAngle * 2 - x);
         if (xOffset < 0) {
           xOffset += 120;
         }
-        xOffset = xOffset % 120;
-        int pixel = (y * 120 + xOffset) * 3;
-        int red = skyColors[pixel];
-        int green = skyColors[pixel + 1];
-        int blue = skyColors[pixel + 2];
-        g.setColor(new Color((int)red, (int)green, (int)blue));
-        g.fillRect(x * 8, y * 8, 8, 8);
+        xOffset = (short)(xOffset % 120);
+        short pixel = (short)((y * 120 + xOffset) * 3);
+        short red = skyColors[pixel];
+        short green = skyColors[pixel + 1];
+        short blue = skyColors[pixel + 2];
+        graphics.setColor(new Color(red, green, blue));
+        graphics.fillRect(x * 8, y * 8, 8, 8);
       }
     }
-    g.dispose();
   }
 
-  private void drawSprites() {
-    Graphics2D g = bufferedImage.createGraphics();
+  private void drawSprites(Graphics2D graphics) {
     double spriteX = sprites.get(0).get(3) - playerX;
     double spriteY = sprites.get(0).get(4) - playerY;
     double spriteZ = sprites.get(0).get(5);
@@ -416,17 +400,15 @@ public class Window extends JPanel implements ActionListener, KeyListener {
     spriteX = (spriteX * 108.0 / spriteY) + (120 / 2);
     spriteY = (spriteZ * 108.0 / spriteY) + (80 / 2);
 
-    int scale = (int)(32 * 80 / b);
+    short scale = (short)(32 * 80 / b);
 
-    for (int x = (int)spriteX - scale / 2; x < (int)spriteX + scale / 2; x++) {
-      for (int y = 0; y < scale; y++) {
+    for (short x = (short)(spriteX - scale / 2); x < spriteX + scale / 2; x++) {
+      for (short y = 0; y < scale; y++) {
         if (x > 0 && x < 120 && b < depth[x]) {
-          g.setColor(Color.yellow);
-          g.fill(new Rectangle2D.Double(x * 8, (spriteY - y) * 8, 8, 8));
+          graphics.setColor(Color.yellow);
+          graphics.fill(new Rectangle2D.Double(x * 8, (spriteY - y) * 8, 8, 8));
         }
       }
     }
-    
-    g.dispose();
   }
 }
